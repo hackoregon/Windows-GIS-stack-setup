a22688031052406ff583bc5065b9d172b4873cba
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2016-10-30 21:56:53 -0700

    not using

--- Linux-EnterpriseDB/install.bash
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-pushd ~/Downloads
-export WHERE=http://get.enterprisedb.com/postgresql/
-export WHAT=postgresql-9.6.1-1-linux-x64.run
-wget --quiet ${WHERE}/${WHAT}
-chmod +x ${WHAT}
-sudo ./${WHAT}
-popd
